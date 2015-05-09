67efd803b23cea5d7687764773668e61f79ad7f7
Tim B <timofey.beletskiy@gmail.com>
2015-05-09 22:28:08 +0100

    inline reading code into plotX.R files

--- plot1.R
+++ plot1.R
@@ -1,6 +1,36 @@
-source("readData.R")
 
-png("plot1.png")
+ # --------------------------------------------------------------------------------
+ # Reading data
+ # --------------------------------------------------------------------------------
+library(lubridate)
+
+read_hpc <- function(){
+    if (!"hpc.zip" %in% dir(".")) {
+        download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", "hpc.zip")
+    }
+    
+    # for sake of speed can first narrow the dataset using "skip" and "nrows" parameters of read.csv;
+    # setting nrows=400000 is enough
+    hpc<-read.csv(unz("hpc.zip", "household_power_consumption.txt"), sep=";", na.strings="?")
+    hpc<-filter(hpc, Date %in% c("1/2/2007","2/2/2007"))
+    hpc<-mutate(hpc, datetime=as.POSIXct(strptime(paste(hpc$Date, hpc$Time), "%d/%m/%Y %H:%M:%S")))
+    return(hpc)
+}
+
+if (!exists("cache")) {
+    print("loading hpc")
+    cache<-read_hpc()
+    hpc<-cache
+} else {
+    print("use cached hpc")
+    hpc<-cache
+}
+ 
+ # --------------------------------------------------------------------------------
+ # Draw
+ # --------------------------------------------------------------------------------
+
+ png("plot1.png")
 par(mfrow=c(1,1))
 hist(hpc$Global_active_power, col="red", ylab="Frequency", xlab="Global Active Power (kilowatts)", main="Global Active Power")
 dev.off()
--- plot4.R
+++ plot4.R
@@ -1,10 +1,41 @@
-source("readData.R")
+
+# --------------------------------------------------------------------------------
+# Reading data
+# --------------------------------------------------------------------------------
+library(lubridate)
+
+read_hpc <- function(){
+    if (!"hpc.zip" %in% dir(".")) {
+        download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", "hpc.zip")
+    }
+    
+    # for sake of speed can first narrow the dataset using "skip" and "nrows" parameters of read.csv;
+    # setting nrows=400000 is enough
+    hpc<-read.csv(unz("hpc.zip", "household_power_consumption.txt"), sep=";", na.strings="?")
+    hpc<-filter(hpc, Date %in% c("1/2/2007","2/2/2007"))
+    hpc<-mutate(hpc, datetime=as.POSIXct(strptime(paste(hpc$Date, hpc$Time), "%d/%m/%Y %H:%M:%S")))
+    return(hpc)
+}
+
+if (!exists("cache")) {
+    print("loading hpc")
+    cache<-read_hpc()
+    hpc<-cache
+} else {
+    print("use cached hpc")
+    hpc<-cache
+}
+
+# --------------------------------------------------------------------------------
+# Draw
+# --------------------------------------------------------------------------------
+
 
 png("plot4.png")
 
 par(mfrow=c(2,2))
 
-plot(hpc$datetime, hpc$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+plot(hpc$datetime, hpc$Global_active_power, type="l", xlab="", ylab="Global Active Power")
 #axis(1, at=c(1, nrow(hpc)*c(.5,1)), labels=c("Thu", "Fri", "Sat"))
 
 plot(hpc$datetime, hpc$Voltage, type="l", ylab="Voltage", xlab="datetime")
